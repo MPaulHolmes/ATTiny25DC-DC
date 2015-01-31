3a229fa0177612277f865a4c8dda754e4b15bdcf
MPaulHolmes <barbiesbla@gmail.com>
2015-01-31 10:02:42 -0700

    Update ATTiny25.c

--- ATTiny25.c
+++ ATTiny25.c
@@ -1,3 +1,4 @@
+// fuse bits are just the factory default.  No need to set them.
 #include <stdio.h>
 #include <avr/wdt.h>
 
@@ -8,31 +9,20 @@ void watchdog_enable();
 int main(void) {
 	watchdog_enable();
 	InitIOPorts();
-	while (1) {
-
-
-		// Set port B3 and B4 to their complements.
-//		PORTB ^= ((1 << PB3) | (1 << PB4));  // When one is low, the other is high.  Swap them here.
+	while (1) {	// this loop runs at 62.5kHz, assuming the attiny25 is running at the factory default of 1MHz.
 		PORTB = 0b10111;
-		asm("nop");asm("nop");asm("nop");
+		asm("nop");asm("nop");asm("nop");   // these 3 nop's are required so that the ports are on and off with equal amounts of time.
 		PORTB = 0b01111;
-
-
 		wdt_reset();
 	}
 	return 0;
 }
 
 void InitIOPorts() {
-	DDRB = 0;			//  all are inputs at this stage.
+	DDRB = 0;		//  all are inputs at this stage.
 	PORTB = 0b11111;  	// turn on all pull up resistors.
 	asm("nop");
 	DDRB = 0b11000; 	// make PB3 and PB4 outputs.
-	//DDRB |= (1 << PB3);    // Data direction register.  Set to 1 to configure as outputs.
-	//DDRB |= (1 << PB4);		// PB4 is also an output.
-	//PORTB
-	//PORTB &= ~(1 << PB3);   // Use PORTB to set the value of pins when they are outputs. PB3 starts as 0.
-	//PORTB |= (1 << PB4);    // Start PB4 as 1.
 }
 
 
